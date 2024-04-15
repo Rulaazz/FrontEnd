e8a0fa3e0e37768e22aa008d3b1e6bdf5b165eb5
Rulaazz <rula.abozaid@gmail.com>
2024-04-15 16:53:58 +0300

    12

---
+++ javascript/q1/type-convertion.js
@@ -0,0 +1,39 @@
+//1
+let num = 123;
+let text = num.toString();
+console.log(text);
+
+//2
+let myString = parseInt('456');
+console.log(myString);
+
+//3
+let str = parseFloat('78.9');
+console.log(str);
+
+//4
+let x=5;
+let y='3';
+let z=x+y;
+console.log(z);
+
+//5
+const value = 0;
+console.log(Boolean(value));
+const value1 = 1;
+console.log(Boolean(value1));
+
+//6
+const i='24';
+console.log(Number(i));
+
+//7
+const t=true;
+const l=false;
+console.log(Number(l));
+console.log(Number(t));
+
+//8
+const v=null;
+console.log(Number(v));
+
